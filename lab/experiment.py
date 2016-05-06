03efdb3398629ddc8791a19cb2d415764418400b
Jim Fleming <jimmy.fleming@gmail.com>
2016-05-06 10:50:05 -0700

    Fill in API endpoints

--- lab/experiment.py
+++ lab/experiment.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 import requests
 import subprocess
 
@@ -8,7 +9,8 @@ ENV = os.environ.get('ENV', 'development')
 if ENV == 'production':
     API_URL = 'https://api.fomoro.com/api/v0.1/experiments/{}/results'
 else:
-    API_URL = 'http://dev.api.fomoro.com/api/0.1/experiments/{}/results'
+    API_URL = 'http://localhost:3000/api/v0.1/experiments/{}/results'
+    # API_URL = 'http://dev.api.fomoro.com/api/0.1/experiments/{}/results'
 
 def get_git_describe():
     return subprocess.check_output(['git', 'describe', '--always', '--dirty', '--abbrev=0'], stderr=subprocess.STDOUT)
@@ -19,7 +21,14 @@ class Experiment(object):
         self.experiment_id = experiment_id
 
         try:
-            self.hash = get_git_describe()
+            self.hash = get_git_describe().strip()
+            self.dirty = False
+
+            if self.hash.endswith('-dirty'):
+                print('WARNING: You have uncommitted changes.')
+                self.hash = self.hash[:-len('-dirty')]
+                self.dirty = True
+
         except subprocess.CalledProcessError as e:
             print('Failed to retrieve current git commit hash.')
             print('Make sure you are running inside of git repo and committed.')
@@ -53,8 +62,19 @@ class Experiment(object):
 
     def report(self, loss, accuracy=None):
         api_url = API_URL.format(self.experiment_id)
+
         json = {
             "hash": self.hash,
-            "loss": loss,
+            "dirty": self.dirty,
+            "loss": random.random(),
         }
-        requests.post(api_url, json=json)
+
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.api_key)
+        }
+
+        r = requests.post(api_url, json=json, headers=headers)
+        if r.status_code == 200:
+            print(r.json())
+        else:
+            print(r.text)
