8c046514e7b5c9c285b00af77b44b50c4858bbcb
Jim Fleming <jimmy.fleming@gmail.com>
2016-05-12 11:41:14 -0700

    Use Fomoro API key

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import random
 
 from lab.experiment import Experiment
 
-API_KEY = os.environ.get('API_KEY')
+FOMORO_API_KEY = os.environ.get('FOMORO_API_KEY')
 
 def main():
-    experiment = Experiment(API_KEY, '6CAc248f')
+    experiment = Experiment(FOMORO_API_KEY, '6CAc248f')
     for i in experiment.iter(range(10)):
         print('step', i)
     experiment.report(random.random(), random.random())
