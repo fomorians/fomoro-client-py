38097d6237a26428b38fb145266f6ffc8a0c5560
Jim Fleming <jimmy.fleming@gmail.com>
2016-05-13 16:17:43 -0700

    Comment code

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import os
 import random
 
+# import a Fomoro `Run` which represents a training run
 from fomoro.run import Run
 
+# read api key from an environment variable
+# (more secure than saving it in the file and version control)
 FOMORO_API_KEY = os.environ.get('FOMORO_API_KEY')
 
-def main():
-    run = Run('6CAc248f', FOMORO_API_KEY, hyperparams={
-        "batch_size": 1
-    }, metadata={
-        "num_parameters": 1e6
-    })
+# create a new `Run` instance, setting appropriate arguments:
+# - project_key (required): your project key
+# - api_key (required): your api key
+# - hyperparams (optional): dictionary of hyperparameters you want to log with the results
+# - metadata (optional): dictionary of additional metadata like number of model parameters
+run = Run(project_key='6CAc248f', api_key=FOMORO_API_KEY, hyperparams={
+    "batch_size": 1
+}, metadata={
+    "num_parameters": 1e6
+})
 
-    for i in run.iter(range(10)):
-        print('step', i)
+# optionally track the number of iterations for progress reporting and step time tracking
+for i in run.iter(range(10)):
+    print('step', i)
 
-    run.report(random.random(), results={
-        "accuracy": random.random()
-    })
-
-if __name__ == '__main__':
-    main()
+# finally, report the results:
+# - loss (required): model loss
+# - results (optional): dictionary of additional results like accuracy, AUC, etc.
+run.report(loss=random.random(), results={
+    "accuracy": random.random()
+})
