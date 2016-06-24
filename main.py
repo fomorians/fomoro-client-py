86a3621d79249922eeff7592c6815ad22ab1d4e2
Jim Fleming <jimmy.fleming@gmail.com>
2016-06-24 13:38:17 -0700

    Rename client

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import random
 
-# import a Fomoro `Results` which represents the results from a training run
-from fomoro.results import Results
+from fomoro import Fomoro
 
 # read api key from an environment variable
 # (more secure than saving it in the file and version control)
@@ -14,19 +13,19 @@ PROJECT_KEY = 'rki7KxaQ'
 # - api_key (required): your api key
 # - hyperparams (optional): dictionary of hyperparameters you want to log with the results
 # - metadata (optional): dictionary of additional metadata like number of model parameters
-results = Results(project_key=PROJECT_KEY, api_key=FOMORO_API_KEY, hyperparams={
+client = Fomoro(project_key=PROJECT_KEY, api_key=FOMORO_API_KEY, hyperparams={
     "batch_size": 1
 }, metadata={
     "num_parameters": 1e6
 })
 
 # optionally track the number of iterations for progress reporting and step time tracking
-for i in results.iter(range(10)):
+for i in client.iter(range(10)):
     print('step', i)
 
 # finally, report the results:
 # - loss (required): model loss
 # - results (optional): dictionary of additional results like accuracy, AUC, etc.
-results.report(loss=random.random(), results={
+client.report(loss=random.random(), results={
     "accuracy": random.random()
 })
